c8af2314c4a1eb30d4f4031b70f330dab9e4b5bd
JAB3000dfs <jacalebak@gmail.com>
2021-06-21 12:36:37 -0400

    test

--- game2.py
+++ game2.py
@@ -1,2 +1,14 @@
 import pygame
 import random
+
+def start_game2():
+    white = (255, 255, 255)
+    colour = (69, 5, 189)
+    black = (0, 0, 0)
+    screen = pygame.display.set_mode((1000, 600))
+    screen.fill(white)
+    while True:
+        pygame.draw.circle(screen, black, (random.randint(250, 751), random.randint(250, 751)), 10)
+        pygame.display.update()
+
+start_game2()
