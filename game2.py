e4c9d2a18101e585611aeb6e205783c82b835ff3
JAB3000dfs <jacalebak@gmail.com>
2021-06-21 12:55:09 -0400

    test

--- game2.py
+++ game2.py
@@ -3,17 +3,18 @@ import random
 import time
 
 def start_game2():
-    white = (255, 255, 255)
-    colour = (69, 5, 189)
-    black = (0, 0, 0)
-    screen = pygame.display.set_mode((1000, 600))
-    screen.fill(white)
+    while True:
+        white = (255, 255, 255)
+        colour = (69, 5, 189)
+        black = (0, 0, 0)
+        screen = pygame.display.set_mode((1000, 600))
+        screen.fill(white)
 
-
-    t_end = time.time() + 4
-    pygame.draw.circle(screen, black, (random.randint(250, 751), random.randint(250, 751)), 10)
-    pygame.display.update()
-    while time.time() < t_end:
-        pass
-    screen.fill(white)
+        t_end = time.time() + 4
+        x = random.randint(250, 350)
+        y = random.randint(250, 350)
+        pygame.draw.circle(screen, black, (x, y), 20)
+        pygame.display.update()
+        while time.time() < t_end:
+            pass
 
