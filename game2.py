3675d1b2a0b310866a04d963f891ab4095ba9121
JAB3000dfs <jacalebak@gmail.com>
2021-06-21 13:00:10 -0400

    test

--- game2.py
+++ game2.py
@@ -3,7 +3,14 @@ import random
 import time
 
 def start_game2():
+    points = 0
+    mouse_x = 0
+    mouse_y = 0
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                mouse_x = pygame.mouse.get_pos()[0]
+                mouse_y = pygame.mouse.get_pos()[1]
         white = (255, 255, 255)
         colour = (69, 5, 189)
         black = (0, 0, 0)
@@ -14,6 +21,9 @@ def start_game2():
         x = random.randint(250, 350)
         y = random.randint(250, 350)
         pygame.draw.circle(screen, black, (x, y), 20)
+        if mouse_x >= x - 20 and mouse_x <= x + 20 and mouse_y >= x - 20 and mouse_y <= x + 20:
+            points += 1
+            print("points")
         pygame.display.update()
         while time.time() < t_end:
             pass
