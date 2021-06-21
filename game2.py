284b04e751a421865b6d2dd86dc2d01c3de99d17
leya-oommen <leya.susan.oommen@gmail.com>
2021-06-21 12:46:29 -0400

    spongebob

--- game2.py
+++ game2.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 def start_game2():
     white = (255, 255, 255)
@@ -7,8 +8,13 @@ def start_game2():
     black = (0, 0, 0)
     screen = pygame.display.set_mode((1000, 600))
     screen.fill(white)
-    for i in range(5):
-        pygame.draw.circle(screen, black, (random.randint(250, 751), random.randint(250, 751)), 10)
-        pygame.display.update()
+
+
+    t_end = time.time() + 4
+    pygame.draw.circle(screen, black, (random.randint(250, 751), random.randint(250, 751)), 10)
+    pygame.display.update()
+    while time.time() < t_end:
+        pass
+    screen.fill(white)
 
 start_game2()
