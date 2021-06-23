395a0c81daf0edd6c4206f0adbf184ee467b1d42
leya-oommen <leya.susan.oommen@gmail.com>
2021-06-23 14:59:03 -0400

    change

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ instructions_font = pygame.font.Font('avgr45w (3).ttf', 20)
 start_screen()
 
 while True:
-    # Getting the mouse x and y values
+
     for event in pygame.event.get():
+        # Getting the mouse x and y values
         if event.type == pygame.MOUSEMOTION:
             mouse_x = pygame.mouse.get_pos()[0]
             mouse_y = pygame.mouse.get_pos()[1]
@@ -32,44 +33,44 @@ while True:
             pygame.quit()
             exit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # If the start button is clicked, start game
-                if 380 < mouse_x < 620 and 395 < mouse_y < 405:
-                    start_game()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the start button is clicked, start game
+            if 380 < mouse_x < 620 and 395 < mouse_y < 405:
+                start_game()
 
-                # If the instructions button is clicked, display instructions
-                elif 370 < mouse_x < 630 and 480 < mouse_y < 520:
-                    screen.fill(pastel_blue)
+            # If the instructions button is clicked, display instructions
+            elif 370 < mouse_x < 630 and 480 < mouse_y < 520:
+                screen.fill(pastel_blue)
 
-                    # Displays instructions
-                    instructions = instructions_font.render("Click on the targets as fast as you can", True, black)
-                    instructions_rectangle = instructions.get_rect()
-                    instructions_rectangle.center = (500, 100)
-                    screen.blit(instructions, instructions_rectangle)
+                # Displays instructions
+                instructions = instructions_font.render("Click on the targets as fast as you can", True, black)
+                instructions_rectangle = instructions.get_rect()
+                instructions_rectangle.center = (500, 100)
+                screen.blit(instructions, instructions_rectangle)
 
-                    instructions2 = instructions_font.render("You have less and less time as the game progress's", True, black)
-                    instructions2_rectangle = instructions2.get_rect()
-                    instructions2_rectangle.center = (500, 150)
-                    screen.blit(instructions2, instructions2_rectangle)
+                instructions2 = instructions_font.render("You have less and less time as the game progress's", True, black)
+                instructions2_rectangle = instructions2.get_rect()
+                instructions2_rectangle.center = (500, 150)
+                screen.blit(instructions2, instructions2_rectangle)
 
-                    instructions3 = instructions_font.render("If you don't click on the targets fast enough you lose a life", True, black)
-                    instructions3_rectangle = instructions3.get_rect()
-                    instructions3_rectangle.center = (500, 200)
-                    screen.blit(instructions3, instructions3_rectangle)
+                instructions3 = instructions_font.render("If you don't click on the targets fast enough you lose a life", True, black)
+                instructions3_rectangle = instructions3.get_rect()
+                instructions3_rectangle.center = (500, 200)
+                screen.blit(instructions3, instructions3_rectangle)
 
-                    instructions4 = instructions_font.render("3 deaths and you're out", True, black)
-                    instructions4_rectangle = instructions4.get_rect()
-                    instructions4_rectangle.center = (500, 250)
-                    screen.blit(instructions4, instructions4_rectangle)
+                instructions4 = instructions_font.render("3 deaths and you're out", True, black)
+                instructions4_rectangle = instructions4.get_rect()
+                instructions4_rectangle.center = (500, 250)
+                screen.blit(instructions4, instructions4_rectangle)
 
-                    back_home = text_font.render(" <--", True, black)
-                    back_home_rectangle = back_home.get_rect()
-                    back_home_rectangle.center = (100, 100)
-                    screen.blit(back_home, back_home_rectangle)
+                back_home = text_font.render(" <--", True, black)
+                back_home_rectangle = back_home.get_rect()
+                back_home_rectangle.center = (100, 100)
+                screen.blit(back_home, back_home_rectangle)
 
-                # If the back arrow is clicked, go to start screen
-                if 50 < mouse_x < 150 and 0 < mouse_y < 600:
-                    start_screen()
+            # If the back arrow is clicked, go to start screen
+            if (50 < mouse_x < 150 and 0 < mouse_y < 600):
+                start_screen()
 
         # Updates the screen
         pygame.display.update()
