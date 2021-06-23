7274c08489800d9e012dfb406fa4848bb2785a89
leya-oommen <leya.susan.oommen@gmail.com>
2021-06-23 14:25:35 -0400

    change

--- functions.py
+++ functions.py
@@ -15,9 +15,9 @@ screen.fill(lavender)
 
 # Font Info
 title_font = pygame.font.Font('Questrial-Regular.ttf', 80)
+title_font.set_bold(True)
 text_font = pygame.font.Font('avgr45w (3).ttf', 40)
 text_font.set_bold(True)
-title_font.set_bold(True)
 
 # Title
 title = title_font.render("TARGET PRACTICE", True, black)
@@ -41,44 +41,46 @@ def start_screen():
     screen.blit(start_button, start_rectangle)
     screen.blit(title, title_rectangle)
 
+# Function that creates game over screen
 def game_over(points):
-    # Sets the window screen
-    black = (0, 0, 0)
-    red = (255, 0, 0)
-    screen = pygame.display.set_mode((1000, 600))
+    # Fills the screen in red
     screen.fill(red)
-    # Initial Page Setup
+
     # Font
     title_font = pygame.font.Font('avgr45w (3).ttf', 100)
     title_font.set_bold(True)
-    text_font = pygame.font.Font('avgr45w (3).ttf', 40)
-    text_font.set_bold(True)
     score_font = pygame.font.Font('avgr45w (3).ttf', 40)
 
     # Title
-    game_over = title_font.render("YOU SUCK", True, black)
+    game_over_title = title_font.render("YOU SUCK", True, black)
+    game_over_rectangle = game_over_title.get_rect()
+    game_over_rectangle.center = (500, 200)
 
-    # Create start button sprit
-    play_again = text_font.render("Play again", True, black)
-    instructions = text_font.render("Instructions", True, black)
-
+    # Score
     score = score_font.render("points:" + str(points), True, black)
     score_rectangle = score.get_rect()
-    game_over_rectangle = game_over.get_rect()
-    game_over_rectangle.center = (500, 200)
     score_rectangle.center = (500, 300)
+
+    # Play again button
+    play_again = text_font.render("Play again", True, black)
     play_again_rectangle = play_again.get_rect()
     play_again_rectangle.center = (500, 400)
+
+    # Instructions button
+    instructions = text_font.render("Instructions", True, black)
     instructions_rectangle = instructions.get_rect()
     instructions_rectangle.center = (500, 500)
 
-    screen.blit(game_over, game_over_rectangle)
+    # Making everything appear
+    screen.blit(game_over_title, game_over_rectangle)
     screen.blit(score, score_rectangle)
     screen.blit(play_again, play_again_rectangle)
     screen.blit(instructions, instructions_rectangle)
 
+    # Updates the screen
     pygame.display.update()
 
+# Start game function
 def start_game():
 
     # Initial variables
@@ -95,16 +97,12 @@ def start_game():
     x = 12000
     y = 12000
 
-
     while True:
+        # Getting the mouse x and y values
         for event in pygame.event.get():
-            # Getting the mouse x and y values
             if event.type == pygame.MOUSEMOTION:
                 mouse_x = pygame.mouse.get_pos()[0]
                 mouse_y = pygame.mouse.get_pos()[1]
-
-        # Timer loop
-        t_end = time.time() + speed
 
         # If the mouse is in the same place as the ball, increase points
         if x + 40 >= mouse_x >= x - 40 and y + 40 >= mouse_y >= y - 40:
@@ -120,40 +118,40 @@ def start_game():
                 game_over(points)
                 break
 
-        # each time the player hits three targets they have 25% less time to click on the next targets
+        # Each time the player hits three targets they have 25% less time to click on the next targets
         if points % 3 == 0:
             speed /= 1.25
 
-        # spawns circle in a random location
+        # Makes the background a new random colour
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        colour = (r, g, b)
+        screen.fill(colour)
+
+        # Spawns circle in a random location
         x = random.randint(20, 980)
         y = random.randint(20, 580)
-
-        three = random.randint(0, 255)
-        two = random.randint(0, 255)
-        one = random.randint(0, 255)
-        colour = (three, two, one)
-
-        screen.fill(colour)
         pygame.draw.circle(screen, black, (x, y), 40)
 
-        # displays the amount of points the player has
+        # Displays the amount of points the player has
         score_font = pygame.font.Font('avgr45w (3).ttf', 20)
         score = score_font.render("Score: " + str(points), True, black)
         score_rectangle = score.get_rect()
         score_rectangle.center = (75, 25)
         screen.blit(score, score_rectangle)
 
-        #displays the amount of lives the player has
+        # Displays the amount of lives the player has left
         lives_font = pygame.font.Font('avgr45w (3).ttf', 20)
         lives = lives_font.render("Lives: " + str(strikes + 1), True, black)
         lives_rectangle = lives.get_rect()
         lives_rectangle.center = (925, 25)
-
         screen.blit(lives, lives_rectangle)
 
-        # updates the screen
+        # Updates the screen
         pygame.display.update()
 
-        # allows the player time to position their mouse on the target
+        # Gives the player time to position their mouse on the target
+        t_end = time.time() + speed
         while time.time() < t_end:
             pass
